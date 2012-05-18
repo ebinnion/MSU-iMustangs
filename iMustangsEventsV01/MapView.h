0ed6d51b0d076eea2eaafd76a5db31fb8560c1fd
Chase Sawyer <ChaseSawyer09@gmail.com>
2012-05-17 22:29:23 -0500

    mess up!

--- iMustangsEventsV01/MapView.h
+++ iMustangsEventsV01/MapView.h
@@ -8,14 +8,20 @@
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+#import "MapView.h"
+#import "TileOverlay.h"
+#import "TileOverlayView.h"
+#import "mapOptionViewController.h"
+#import "CoreLocation/CoreLocation.h"
+#import "MyAnnotation.h"
+#import "ActionSheetPicker.h"
 
 @interface MapView : UIViewController <MKMapViewDelegate> {
-    
     IBOutlet MKMapView *map;
     NSMutableString *navFromView;
     NSInteger selectedIndex;
-    
 }
+
 @property (nonatomic, assign) NSInteger selectedIndex;
 @property (nonatomic, strong) NSMutableString *navFromView;
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *mapOptionButton;
